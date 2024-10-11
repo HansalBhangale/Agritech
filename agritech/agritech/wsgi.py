d99212bbef57bba22981946113127b7b816d73fd
HansalBhangale <bhangalehansal@gmail.com>
2024-10-11 16:13:59 +0530

    Second commit

--- agritech/agritech/wsgi.py
+++ agritech/agritech/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agritech.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agritech.agritech.settings')
 
 application = get_wsgi_application()
