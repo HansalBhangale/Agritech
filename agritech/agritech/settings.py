fe6394ed4682465503db4a9c99554e0c605b0bbd
HansalBhangale <bhangalehansal@gmail.com>
2024-10-12 18:00:29 +0530

    Fourth

--- agritech/agritech/settings.py
+++ agritech/agritech/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/path/to/Agritech/agritech/static/'
+STATIC_ROOT = 'static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
